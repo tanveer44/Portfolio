d538ee27e4b694cb8d3fde4012ac1c9f2f4bfdc3
Tanveer shaik <tanveer442004@gmail.com>
2024-03-04 22:03:51 +0530

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+
+    var typed = new Typed(".text", {
+        strings: ["Frontend Developer", "Full Stack Developer", "web Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+function showAlert(){
+
+    alert("Thank you for your message!");
+}
